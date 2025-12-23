d4f3f21dabb838d9acb28137382d2bf23010960e
unknown <hemant110800@gmail.com>
2025-12-23 18:41:21 +0530

    env fixes

--- frontend/src/utils/http_methods.js
+++ frontend/src/utils/http_methods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const url = import.meta.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
+const url = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
 
 
 export async function getToken(data) {
